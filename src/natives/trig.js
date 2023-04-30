134a0a4033128ead51c6e5019f672b9f8151f1d1
Efnilite <35348263+Efnilite@users.noreply.github.com>
2023-04-30 22:48:01 +0200

    Removed magic number, reduced code repetition

--- src/natives/trig.js
+++ src/natives/trig.js
@@ -1,98 +1,77 @@
-import {Fraction} from "../types/fraction.js";
-import {registerNativeFns} from "../element/fn.js";
+import {Fraction} from "../types/fraction.js"
+import {registerNativeFns} from "../element/fn.js"
+import {assert} from "../element/util.js"
 
-// todo explain magic numbers
+// todo consolidate!
+
+const assertIsFraction = x => assert(x instanceof Fraction, "Function only supports numeric type (Fraction)")
+
+const RADS_PER_DEGREE = 0.01745329
 
 let trigFuncs = {
     cos: (angle, radian = true) => {
-        if (!(angle instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        let radAngle = (radian ? angle.evaluate() : (angle.evaluate() * 0.0174533))
-        return new Fraction(Math.cos(radAngle));
+        assertIsFraction(angle)
+        let radAngle = (radian ? angle.evaluate() : (angle.evaluate() * RADS_PER_DEGREE))
+        return new Fraction(Math.cos(radAngle))
     },
     sin: (angle, radian = true) => {
-        if (!(angle instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        let radAngle = (radian ? angle.evaluate() : (angle.evaluate() * 0.0174533))
-        return new Fraction(Math.sin(radAngle));
+        assertIsFraction(angle)
+        let radAngle = (radian ? angle.evaluate() : (angle.evaluate() * RADS_PER_DEGREE))
+        return new Fraction(Math.sin(radAngle))
     },
     tan: (angle, radian = true) => {
-        if (!(angle instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        let radAngle = (radian ? angle.evaluate() : (angle.evaluate() * 0.0174533))
-        return new Fraction(Math.tan(radAngle));
+        assertIsFraction(angle)
+        let radAngle = (radian ? angle.evaluate() : (angle.evaluate() * RADS_PER_DEGREE))
+        return new Fraction(Math.tan(radAngle))
     },
     cot: (angle, radian = true) => {
-        if (!(angle instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        let radAngle = (radian ? angle.evaluate() : (angle.evaluate() * 0.0174533))
-        return new Fraction(1 / Math.tan(radAngle));
+        assertIsFraction(angle)
+        let radAngle = (radian ? angle.evaluate() : (angle.evaluate() * RADS_PER_DEGREE))
+        return new Fraction(1 / Math.tan(radAngle))
     },
     arccos: (angle, radian = true) => {
-        if (!(angle instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        let radAngle = Math.acos(angle.evaluate());
-        return new Fraction((radian ? radAngle : radAngle / 0.0174533));
+        assertIsFraction(angle)
+        let radAngle = Math.acos(angle.evaluate())
+        return new Fraction((radian ? radAngle : radAngle / RADS_PER_DEGREE))
     },
     arcsin: (angle, radian = true) => {
-        if (!(angle instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        let radAngle = Math.asin(angle.evaluate());
-        return new Fraction((radian ? radAngle : radAngle / 0.0174533));
+        assertIsFraction(angle)
+        let radAngle = Math.asin(angle.evaluate())
+        return new Fraction((radian ? radAngle : radAngle / RADS_PER_DEGREE))
     },
     arctan: (angle, radian = true) => {
-        if (!(angle instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        let radAngle = Math.atan(angle.evaluate());
-        return new Fraction((radian ? radAngle : radAngle / 0.0174533));
+        assertIsFraction(angle)
+        let radAngle = Math.atan(angle.evaluate())
+        return new Fraction((radian ? radAngle : radAngle / RADS_PER_DEGREE))
     },
     arccot: (angle, radian = true) => {
-        if (!(angle instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        let radAngle = Math.atan(1 / angle.evaluate());
-        return new Fraction((radian ? radAngle : radAngle / 0.0174533));
+        assertIsFraction(angle)
+        let radAngle = Math.atan(1 / angle.evaluate())
+        return new Fraction((radian ? radAngle : radAngle / RADS_PER_DEGREE))
     },
     rad: (angle) => {
-        if (!(angle instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        return new Fraction((angle.evaluate()) * 0.0174533);
+        assertIsFraction(angle)
+        return new Fraction((angle.evaluate()) * RADS_PER_DEGREE)
     },
     sinh: (num, radians = true) => {
-        if (!(num instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        let radAngle = (radians ? num.evaluate : (num.evaluate() * 0.0174533));
+        assertIsFraction(num)
+        let radAngle = (radians ? num.evaluate : (num.evaluate() * RADS_PER_DEGREE))
         return new Fraction(Math.sinh(radAngle))
     },
     cosh: (num, radians = true) => {
-        if (!(num instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        let radAngle = (radians ? num.evaluate : (num.evaluate() * 0.0174533));
+        assertIsFraction(num)
+        let radAngle = (radians ? num.evaluate : (num.evaluate() * RADS_PER_DEGREE))
         return new Fraction(Math.cosh(radAngle))
     },
     tanh: (num, radians = true) => {
-        if (!(num instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        let radAngle = (radians ? num.evaluate : (num.evaluate() * 0.0174533));
+        assertIsFraction(num)
+        let radAngle = (radians ? num.evaluate : (num.evaluate() * RADS_PER_DEGREE))
         return new Fraction(Math.tanh(radAngle))
     },
     deg: (num) => {
-        if (!(num instanceof Fraction)) {
-            throw new TypeError('Function only supports numeric type (Fraction)');
-        }
-        return new Fraction((num.evaluate()) / 0.0174533);
+        assertIsFraction(num)
+        return new Fraction((num.evaluate()) / RADS_PER_DEGREE)
     }
 }
 
-registerNativeFns(trigFuncs);
+registerNativeFns(trigFuncs)
